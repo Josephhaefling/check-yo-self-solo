3b7a982ef5e046920e39e91ca5ca3202fd3fbfe5
Joe Haefling <josephhaefling@gmail.com>
2020-02-28 17:53:12 -0700

    Add task functionality

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-var navInput = document.querySelector('#all-nav-btn');
-var mainHeader = document.querySelector('#main-header')
+var navListener = document.querySelector('#nav-listener');
+var mainHeader = document.querySelector('#js-main-header');
 
 
-navInput.addEventListener('click', btnClicked);
-// mainHeader.addEventListener('click', search);
+navListener.addEventListener('click', btnClicked);
+mainHeader.addEventListener('click', btnClicked);
 
 function btnClicked () {
-  var target = event.target ;
-  if (target.classList[0] === 'js-nav-btn') {
+  var target = event.target;
+  if (target.classList[0] ==='js-nav-btn') {
     determineBtn(target);
+  } else if (target.id ==='search-button'){
+    console.log('search');
   }
 }
 
 function determineBtn(target) {
-  if (target.id === 'add-btn-img') {
-    console.log('add');
+  if (target.id === 'add-btn') {
+    addTask()
   } else if (target.id === 'make-list'){
   console.log('make');
-} else if (target.id === 'clear-all'){
+  } else if (target.id === 'clear-all'){
     console.log('clear');
   } else if (target.id === 'ugency-filter')
   console.log('urgent');
+}
+
+function addTask() {
+  var newTaskSpace = document.querySelector('#new-task-space');
+  var newTask = document.querySelector('#task-item-input');
+  if (newTask.value) {
+    newTaskSpace.insertAdjacentHTML('afterbegin', `<div><img id="small-image" src="assets/delete.svg">${newTask.value}</div>`);
+  }
 }
